e116f43621f90a503061e39840a8afe96309b8b1
Ali.Ghodrat <ght.ali@hotmail.com>
2016-10-13 23:46:25 +0200

    Player.java first demo.

--- CDIO2/src/spil/Player.java
+++ CDIO2/src/spil/Player.java
@@ -1,69 +1,81 @@
 package spil;
 
 public class Player{
-	
+
 	private int points = 0;
 	private String playerName;
-	private BankAccount bankAccount;
-	private int id;
-	private int nextId;
-	
+	private BankAccount bankAccount; // instead of Bank I used BankAccount
+	private int id; // I suggest that we use the ID in the GAmeboard
+	private int nextId; // I suggest that we use nextId in the Gameboard
+
 	/**
 	 * Create a new Bank Account for a new player
-	 * @set Player banck account to 1000 for the first time
+	 * @set Player accounts balance to 1000 for the first time
 	 */
 	public Player() {
-		
+
 		bankAccount = new BankAccount(1000);
-		
+
 	}
 	
-	public BankAccount getBankAccount()
-	{
+	// used set methode to set player names
+
+	public void setPlayerName(String playerName){
+		this.playerName = playerName;
+	}
+
+	// used get to return player bank account	
+	public BankAccount getBankAccount(){
 		return bankAccount;
 	}
-	
-	public String getplayerName() {
+
+	//USed get methode to return player name
+	public String getPlayerName(){
 		return playerName;
 	}
-	
-	public int getID() {
-	return id;
-}
-	
-	
-	
-	
-//	public String toString() {
-//		return name + " har nu " + points + " points.\n";
-//	}
+	// get method for getting objects! ID
+	public int getID(){
+		return id;
+	}
+	// used get method to return next object id!!
+	public int getNextID(){
+		
+		return nextId;
+	}
 	
 
-//	/**
-//	 * Gets point on player object
-//	 * @return int
-//	 */
-//	public int getPoints() {
-//		return points;
-//	}
 
-//	/**
-//	 * Sets points on player object
-//	 * @param points int
-//	 */
-//	public void setPoints(int points) {
-//		this.points = points;
-//	}
-//
-//	/**
-//	 * Appends point on player object
-//	 * @param points int
-//	 */
-//	public void addPoints(int points) {
-//		this.points = this.points + points;
-//	}
 
-	
+	//	public String toString() {
+	//		return name + " har nu " + points + " points.\n";
+	//	}
+
+
+	//	/**
+	//	 * Gets point on player object
+	//	 * @return int
+	//	 */
+	//	public int getPoints() {
+	//		return points;
+	//	}
+
+	//	/**
+	//	 * Sets points on player object
+	//	 * @param points int
+	//	 */
+	//	public void setPoints(int points) {
+	//		this.points = points;
+	//	}
+	//
+	//	/**
+	//	 * Appends point on player object
+	//	 * @param points int
+	//	 */
+	//	public void addPoints(int points) {
+	//		this.points = this.points + points;
+	//	}
+
+
 	/**
 	 * Returns player name from player object
 	 * @return String
